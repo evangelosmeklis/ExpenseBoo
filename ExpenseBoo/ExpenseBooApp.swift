969b067a5f0b808df1804a839c95a012c0ba2ab8
Evangelos Meklis <vmeklis@hotmail.com>
2025-08-03 18:08:18 +0300

    Initial Commit

---
+++ ExpenseBoo/ExpenseBooApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseBooApp.swift
+//  ExpenseBoo
+//
+//  Created by Evangelos Meklis on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseBooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExpenseBooTests/ExpenseBooTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseBooTests.swift
+//  ExpenseBooTests
+//
+//  Created by Evangelos Meklis on 3/8/25.
+//
+
+import Testing
+@testable import ExpenseBoo
+
+struct ExpenseBooTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
